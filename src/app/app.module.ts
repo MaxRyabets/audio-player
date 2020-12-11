1da5a183d640b078adeaab3d42cbcbe8e3ced1ea
Максим Рябец <maksaxel2@gmail.com>
2020-12-11 09:51:20 +0200

    feat(audio-list): add component for audio list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AudioPlayerComponent } from './audio-player/audio-player.component';
+import { AudioListComponent } from './audio-list/audio-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AudioPlayerComponent
+    AudioPlayerComponent,
+    AudioListComponent
   ],
   imports: [
     BrowserModule
