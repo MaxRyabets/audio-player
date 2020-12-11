d876e01128a53eb2bdc0078d2d504c23d5921da9
Максим Рябец <maksaxel2@gmail.com>
2020-12-11 09:40:18 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AudioPlayersComponent } from './audio-players/audio-players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AudioPlayersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/audio-players/audio-players.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-players',
+  templateUrl: './audio-players.component.html',
+  styleUrls: ['./audio-players.component.scss']
+})
+export class AudioPlayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
